9d4a9075896d0c91124e6717bf31fddf4aef530c
Achint <kumarachint22@gmail.com>
2021-12-04 22:26:34 +0530

    added linked list all insertion

--- c/link list/linked_list.c
+++ c/link list/linked_list.c
@@ -8,39 +8,73 @@ Code, Compile, Run and Debug online from anywhere in world.
 *******************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
-struct list{                        //decl struct
-    int data;
-    struct list *link;
+ 
+// A linked list node
+struct list {
+  int data;
+  struct list *next;
 };
-typedef struct list node;               //changeing name
-void insert(node **, int );             //dec proto
-void display(node *);
-    
-int main(){                             //main fun
+typedef struct list node; 
+
+void insert_first(node **head_ref, int new_data){
+    node* new_node = (node*) malloc(sizeof(node));
+    new_node->data  = new_data;
+    new_node->next = *head_ref;
+    *head_ref = new_node;
+}
+void insert_bet(node **head_ref, int new_data){
+    int count;
+    if (head_ref== NULL){
+      printf("the given head cannot be NULL");
+      return;
+    }else{
+        node *temt = *head_ref;
+        node* new_node =(node*) malloc(sizeof(node));
+        new_node->data  = new_data;
+        new_node->next = NULL;
+        printf("Enter the position\n");
+        scanf("%d", &count);
+        for (int i = 0; i < count; i++){
+            temt = temt -> next;
+        }
+        new_node -> next = temt -> next;
+        temt -> next = new_node;
+    }
+    return;
+}
+void at_last(node** head_ref, int new_data){
+    node* new_node = (node*) malloc(sizeof(node));
+    node *last = *head_ref; 
+    new_node->data  = new_data;
+    new_node->next = NULL;
+    if (*head_ref == NULL){
+       *head_ref = new_node;
+       return;
+    }
+ 
+    while (last->next != NULL){
+        last = last->next;
+    }
+    last->next = new_node;
+    return;
+}
+ 
+void display(node *list){
+  while (list != NULL){
+     printf(" %d ", list->data);
+     list = list->next;
+  }
+}
+ 
+int main(){
     node *head = NULL;
-    insert(&head, 50);                  // call func
-    insert(&head, 40);
+    insert_first(&head, 1);
+    at_last(&head, 2);
+    insert_first(&head, 3);
+    at_last(&head, 4);
+    insert_first(&head, 6);
+    insert_bet(&head, 5);
+    printf("\nElement of linked list is:\f ");
     display(head);
     return 0;
-}
-void insert(node **head_ref, int num){                      // defining fun
-    node *p = (node *)malloc(sizeof(node));                 // creating new node 
-    p->data = num;                                          //filling data in linked list 
-    p->link = NULL;
-    
-    if (*head_ref == NULL){                             // condition assining 1st node address to pointer head
-        *head_ref = p;
-    }else {
-        node *temt = *head_ref;                     // creating new pointer to store 1st node address 
-    while(temt->link != NULL){                      // traverse in linked list to add new node to teh last
-        temt = temt -> link;
-    }
-    temt -> link = p;
-    }
-}
-void display(node * list){                          // displaing the data of linked list
-    while (list != NULL){
-        printf("%d  ", list->data);
-        list = list -> link;
-    }
 }
